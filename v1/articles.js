d0058aa35839f708dc8c13ec0de427dc557c7c04
Thet Win Aung <minthit21dj@gmail.com>
2018-07-27 23:52:28 +0900

    Update fetchList

--- v1/articles.js
+++ v1/articles.js
@@ -4,7 +4,11 @@ const Dropbox = require('dropbox').Dropbox;
 const dbx = new Dropbox({ accessToken: dbToken });
 
 module.exports = async (req, res) => {
-  const response = await dbx.filesListFolder({path: '/something'})
+  const response = await dbx.filesListFolder({
+    path: '/something',
+    recursive: true,
+    limit: 10
+  })
   console.log('response', response);
 
   const data = {
@@ -16,5 +20,26 @@ module.exports = async (req, res) => {
   }
 
   return response;
+}
+
+const loadFiles = async () => {
+  let files = []
+  let hasMore = true
+  while (hasMore) {
+    hasMore = false
+    try {
+      const response = await dbx.filesListFolder({
+        path: '/something',
+        recursive: true,
+        limit: 10
+      })
+      console.log('response', response);
+      files = files.concat(response.entries || [])
+      hasMore = response.has_more
+    } catch(err) {
+      console.error('loadFiles error', err)
+    }
+  }
+
 }
 
